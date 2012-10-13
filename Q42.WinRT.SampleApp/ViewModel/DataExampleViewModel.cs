22852e36bcdc57827069d14940baba048d946f53
Michiel Post <michiel@michielpost.nl>
2012-10-13 12:29:53 +0200

    Better naming in sample app

--- Q42.WinRT.SampleApp/ViewModel/DataExampleViewModel.cs
+++ Q42.WinRT.SampleApp/ViewModel/DataExampleViewModel.cs
@@ -23,33 +23,33 @@ namespace Q42.WinRT.SampleApp.ViewModel
             }
         }
 
-        private string _result6;
+        private string _cacheRefreshResult;
 
-        public string Result6
+        public string CacheRefreshResult
         {
-            get { return _result6; }
+            get { return _cacheRefreshResult; }
             set
             {
-                _result6 = value;
-                RaisePropertyChanged("Result6");
+                _cacheRefreshResult = value;
+                RaisePropertyChanged("CacheRefreshResult");
             }
         }
         
 
-        public DataLoader MyDataLoader { get; set; }
-        public DataLoader MyDataLoader2 { get; set; }
-        public DataLoader MyDataLoader3 { get; set; }
-        public DataLoader MyDataLoader4 { get; set; }
-        public DataLoader MyDataLoader5 { get; set; }
-        public DataLoader MyDataLoader6 { get; set; }
+        public DataLoader StartLongRunningDataLoader { get; set; }
+        public DataLoader StartLongRunningWithExceptionDataLoader { get; set; }
+        public DataLoader CacheDataLoader { get; set; }
+        public DataLoader CacheWithExceptionDataLoader { get; set; }
+        public DataLoader SourceABDataLoader { get; set; }
+        public DataLoader CacheRefreshDataLoader { get; set; }
 
 
-        public RelayCommand Button1Command { get; set; }
-        public RelayCommand Button2Command { get; set; }
-        public RelayCommand Button3Command { get; set; }
-        public RelayCommand Button4Command { get; set; }
-        public RelayCommand Button5Command { get; set; }
-        public RelayCommand Button6Command { get; set; }
+        public RelayCommand StartLongRunningCommand { get; set; }
+        public RelayCommand StartLongRunningWithExceptionCommand { get; set; }
+        public RelayCommand CacheCommand { get; set; }
+        public RelayCommand CacheWithExceptionCommand { get; set; }
+        public RelayCommand SourceABCommand { get; set; }
+        public RelayCommand CacheRefreshCommand { get; set; }
         public RelayCommand ClearCacheCommand { get; set; }
 
         public RelayCommand ClearWebDataCacheCommand { get; set; }
@@ -57,20 +57,20 @@ namespace Q42.WinRT.SampleApp.ViewModel
 
         public DataExampleViewModel()
         {
-            MyDataLoader = new DataLoader();
-            MyDataLoader2 = new DataLoader(true); //swallow exceptions
-            MyDataLoader3 = new DataLoader();
-            MyDataLoader4 = new DataLoader(true); //swallow exceptions
-            MyDataLoader5 = new DataLoader(true); //swallow exceptions
-            MyDataLoader6 = new DataLoader(true); //swallow exceptions
+            StartLongRunningDataLoader = new DataLoader();
+            StartLongRunningWithExceptionDataLoader = new DataLoader(true); //swallow exceptions
+            CacheDataLoader = new DataLoader();
+            CacheWithExceptionDataLoader = new DataLoader(true); //swallow exceptions
+            SourceABDataLoader = new DataLoader(true); //swallow exceptions
+            CacheRefreshDataLoader = new DataLoader(true); //swallow exceptions
 
 
-            Button1Command = new RelayCommand(() => Button1Action());
-            Button2Command = new RelayCommand(() => Button2Action());
-            Button3Command = new RelayCommand(() => Button3Action());
-            Button4Command = new RelayCommand(() => Button4Action());
-            Button5Command = new RelayCommand(() => Button5Action());
-            Button6Command = new RelayCommand(() => Button6Action());
+            StartLongRunningCommand = new RelayCommand(() => StartLongRunningAction());
+            StartLongRunningWithExceptionCommand = new RelayCommand(() => StartLongRunningWithExceptionAction());
+            CacheCommand = new RelayCommand(() => CacheAction());
+            CacheWithExceptionCommand = new RelayCommand(() => CacheWithExceptionAction());
+            SourceABCommand = new RelayCommand(() => SourceABAction());
+            CacheRefreshCommand = new RelayCommand(() => CacheRefreshAction());
             ClearCacheCommand = new RelayCommand(() => ClearCacheAction());
 
             ClearWebDataCacheCommand = new RelayCommand(() => ClearWebDataCacheCommandAction());
@@ -78,63 +78,82 @@ namespace Q42.WinRT.SampleApp.ViewModel
 
         }
 
-      
-        private async void Button1Action()
+        /// <summary>
+        /// Shows data loader for long running operation
+        /// </summary>
+        private async void StartLongRunningAction()
         {
             //Fire task
             //Will show loader in the UI
-            string result = await MyDataLoader.LoadAsync(() => LongRunningOperation());
+            string result = await StartLongRunningDataLoader.LoadAsync(() => LongRunningOperation());
         }
 
-        private async void Button2Action()
+        /// <summary>
+        /// Long running operation with exception as result
+        /// </summary>
+        private async void StartLongRunningWithExceptionAction()
         {
             //Fire task
             //Will show loader in the UI
-            string result = await MyDataLoader2.LoadAsync(() => LongRunningOperationWithException());
+            string result = await StartLongRunningWithExceptionDataLoader.LoadAsync(() => LongRunningOperationWithException());
         }
 
-        private async void Button3Action()
+        /// <summary>
+        /// Get data from cache (or insert into cache if it's not there yet)
+        /// </summary>
+        private async void CacheAction()
         {
             //Fire task
             //Will show loader in the UI
-            string result = await MyDataLoader3.LoadAsync(() => JsonCache.GetAsync("samplekey", () => LongRunningOperation()));
+            string result = await CacheDataLoader.LoadAsync(() => JsonCache.GetAsync("samplekey", () => LongRunningOperation()));
         }
 
-        private async void Button4Action()
+
+        /// <summary>
+        /// Get from cache with Exception result
+        /// </summary>
+        private async void CacheWithExceptionAction()
         {
             //Fire task
             //Will show loader in the UI
-            //string result = await MySecondDataLoader.Load(() => LongRunningOperationWithException());
-
-            string result = await MyDataLoader4.LoadAsync(() => JsonCache.GetAsync("samplekey_exception", () => LongRunningOperationWithException(), expireDate: DateTime.Now.AddDays(1)));
+            string result = await CacheWithExceptionDataLoader.LoadAsync(() => JsonCache.GetAsync("samplekey_exception", () => LongRunningOperationWithException(), expireDate: DateTime.Now.AddDays(1)));
 
 
         }
 
-        private void Button5Action()
+        /// <summary>
+        /// First load data from source A then source B
+        /// </summary>
+        private void SourceABAction()
         {
             Result = string.Empty;
 
             //Fire task
             //Will show loader in the UI
-            MyDataLoader5.LoadCacheThenRefreshAsync(() => LongRunningOperation("first result"), () => LongRunningOperation("refresh result"), x =>
+            SourceABDataLoader.LoadCacheThenRefreshAsync(() => LongRunningOperation("first result"), () => LongRunningOperation("refresh result"), x =>
                 {
                     Result = x;
                 });
         }
 
-        private void Button6Action()
+        /// <summary>
+        /// First return cached result, then refresh this result with live value
+        /// </summary>
+        private void CacheRefreshAction()
         {
-            Result6 = string.Empty;
+            CacheRefreshResult = string.Empty;
 
             //Fire task
             //Will show loader in the UI
-            MyDataLoader6.LoadCacheThenRefreshAsync(() => JsonCache.GetFromCache<string>("key6"), () => JsonCache.GetAsync("key6", () => LongRunningOperation(DateTime.Now.Second.ToString()), expireDate: DateTime.Now.AddDays(1), forceRefresh: true), x =>
+            CacheRefreshDataLoader.LoadCacheThenRefreshAsync(() => JsonCache.GetFromCache<string>("key6"), () => JsonCache.GetAsync("key6", () => LongRunningOperation(DateTime.Now.Second.ToString()), expireDate: DateTime.Now.AddDays(1), forceRefresh: true), x =>
             {
-                Result6 = x;
+                CacheRefreshResult = x;
             });
         }
 
+        /// <summary>
+        /// Load uri and set data in cache
+        /// </summary>
         private async void GetUriCommandAction()
         {
             var result = await WebDataCache.GetAsync(new Uri("http://microsoft.com"), forceGet: false);
@@ -144,12 +163,17 @@ namespace Q42.WinRT.SampleApp.ViewModel
             }
         }
 
-
+        /// <summary>
+        /// Clear WebDataCache
+        /// </summary>
         private void ClearWebDataCacheCommandAction()
         {
             WebDataCache.ClearAll();
         }
 
+        /// <summary>
+        /// Clears the JsonCache
+        /// </summary>
         private void ClearCacheAction()
         {
             JsonCache.ClearAll();
@@ -157,6 +181,10 @@ namespace Q42.WinRT.SampleApp.ViewModel
 
 
 
+        /// <summary>
+        /// Sample long running operation
+        /// </summary>
+        /// <returns></returns>
         private async Task<string> LongRunningOperation()
         {
             //For example, webservice call
@@ -166,6 +194,11 @@ namespace Q42.WinRT.SampleApp.ViewModel
             return "result";
         }
 
+        /// <summary>
+        /// Sample long running operation
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         private async Task<string> LongRunningOperation(string input)
         {
             //For example, webservice call
@@ -175,6 +208,10 @@ namespace Q42.WinRT.SampleApp.ViewModel
             return "Input: " + input;
         }
 
+        /// <summary>
+        /// Sample long running operation
+        /// </summary>
+        /// <returns></returns>
         private async Task<string> LongRunningOperationWithException()
         {
             //For example, webservice call
@@ -183,5 +220,6 @@ namespace Q42.WinRT.SampleApp.ViewModel
 
             throw new Exception("Error");
         }
+
     }
 }
